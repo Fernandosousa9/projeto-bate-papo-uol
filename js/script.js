ac8ac6fc74c25771b0cbd6ff28330130ed2305b6
Fernandosousa9 <fernando.castro10.master@gmail.com>
2022-04-17 15:14:07 -0300

    alteração no layout

--- js/script.js
+++ js/script.js
@@ -1,56 +1,12 @@
-let mensagens = [];
+const url = "https://mock-api.driven.com.br/api/v6/uol/participants";
+const urlStatus = "https://mock-api.driven.com.br/api/v6/uol/status";
+const urlMessages = "https://mock-api.driven.com.br/api/v6/uol/messages";
 
-//console.log(pegarMensagem());
-pegarMensagem();
-
-
-
-function pegarMensagem(){
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
-
-    //callback
-    promise.then(carregarDados);
-}
-
-function carregarDados(response){
-    mensagens = response.data;
-    renderizarMensagens();
-}
-
-function renderizarMensagens(){
-    const ulMensagens = document.querySelector(".mensagens");
-    ulMensagens.innerHTML = "";
-
-    for(let i = 0; i < mensagens.length; i++){
-        ulMensagens.innerHTML += `
-        <li>
-            ${mensagens[i].name}
-        </li>
-        `;
-    }
-}
-
-function adicionarMensagem(){
-    const name = document.querySelector(".text-box").value;
-
-    const novaMensagem = {
-        name: name,
-    };
-
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", novaMensagem);
-
-    promise.then(tratarSucesso);
-
-    promise.catch(tratarFalha);
-}
-
-function tratarFalha(){
-    alert("Deu ruim");
-    console.log(error.response);
-}
-
-function tratarSucesso(){
-    alert("Mensagem enviada com sucesso");
-}
-
-promise.then(pegarMensagem);
+const nome = {};
+const mensagemEnviar = {
+    from: "",
+    to: "Todos",
+    text: "",
+    type: "message" // ou "private_message" para o bônus
+};
+let ultimaMensagem;
